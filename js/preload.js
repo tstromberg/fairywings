554e6d09bbdefe55c7ef14e5fc414f1b45b1a641
Thomas Stromberg <tstromberg@tstromberg-macbookpro2.roam.corp.google.com>
2017-01-02 10:56:32 -0800

    Initial commit: based on Phaser tutorial.

---
+++ js/preload.js
@@ -0,0 +1,6 @@
+function preload() {
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('fruit', 'assets/fruit.png');
+    game.load.spritesheet('fairy', 'assets/fairy.png', 32, 48);
+}
---
+++ js/update.js
@@ -0,0 +1,27 @@
+
+function update() {
+    // Physics is hard.
+    game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    // Handle character movement.
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    }  else if (cursors.right.isDown) {
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    } else {
+        player.animations.stop();
+        player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -350;
+    }
+}
